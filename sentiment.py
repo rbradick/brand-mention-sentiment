e3af03f3011b7c2d76f18d6bba9f068db24765a9
rbradick <rbradick@gmail.com>
2025-07-09 10:17:00 -0700

    Delete sentiment.py

--- sentiment.py
+++
@@ -1,6 +0,0 @@
-from transformers import pipeline
-
-def analyze_sentiment(texts):
-    sentiment_pipeline = pipeline("sentiment-analysis")
-    results = sentiment_pipeline(texts)
-    return results
